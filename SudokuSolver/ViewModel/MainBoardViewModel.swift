cde248aa6207b947c32c59f1bc2765a9a3de0a32
tas5521 <tsbs11152@gmail.com>
2024-03-03 22:18:46 +0900

    #9 コメントを追加

--- SudokuSolver/ViewModel/MainBoardViewModel.swift
+++ SudokuSolver/ViewModel/MainBoardViewModel.swift
@@ -36,14 +36,19 @@ class MainBoardViewModel {
     
     // 数独の盤面に数字を入力するメソッド
     func enterNumberOnBoard(row: Int, column: Int) {
+        // ボタンタイプの数値を取得
         let number = selectedButton.rawValue
+        // ボタンタイプが.hintもしくは.startの場合は何もしないで終了
         guard number < 10 else { return }
+        // 盤面の状態を記録
         pushSudokuIntoStack()
+        // 指定されたセルに数字を配置
         sudoku[row][column] = number
     } // enterToBoard ここまで
     
     // 現在の数独をスタックに格納するメソッド
     private func pushSudokuIntoStack() {
+        // 数独のスタックに現在の状態を記録
         sudokuStack.append(sudoku)
     } // pushIntoStackここまで
     
@@ -55,7 +60,9 @@ class MainBoardViewModel {
     
     // 盤面の数字を全てクリアするメソッド
     func clearAll() {
+        // 盤面の状態を記録
         pushSudokuIntoStack()
+        // 現在の数独の状態に空の数独を渡す
         sudoku = emptySudoku
     } // clearAll ここまで
 } // MainBoardViewModel
