6d56fecafc65413d1f478880eab6fccdc49bb2b0
tas5521 <tsbs11152@gmail.com>
2024-03-10 13:23:46 +0900

    #31 画像のドラッグを実装

--- SudokuSolver/View/DragAndPinchImage.swift
+++ SudokuSolver/View/DragAndPinchImage.swift
@@ -11,7 +11,13 @@ struct DragAndPinchImage: View {
     // 画像を保持する変数
     let image: UIImage
     
-    // 画像のスケーリングに関わる変数
+    // 画像のドラッグに関わる変数
+    // 現在のオフセットを保持する変数
+    @State var offset: CGSize = .zero
+    // オフセットを調節した後の値
+    @State var lastOffset: CGSize = .zero
+    
+    // 画像の拡大縮小に関わる変数
     // 現在のスケールを保持する変数
     @State private var scale: CGFloat = 1.0
     // スケールを調節した後の値
@@ -36,13 +42,27 @@ struct DragAndPinchImage: View {
             .scaledToFill()
         // 幅高さ280に指定
             .frame(width: 280, height: 280)
+        // ドラッグ位置を調節
+            .offset(offset)
         // スケールを調節
             .scaleEffect(scale)
         // 回転を調節
             .rotationEffect(angle, anchor: .center)
         // ジェスチャーを指定
+            .gesture(dragGesture)
             .gesture(SimultaneousGesture(scaleGuesture, rotateGesture))
     } // body ここまで
+    
+    // 画像のドラッグ
+    var dragGesture: some Gesture {
+        DragGesture()
+            .onChanged {
+                offset = CGSize(width: lastOffset.width + $0.translation.width, height: lastOffset.height + $0.translation.height)
+            } // onChanged ここまで
+            .onEnded { _ in
+                lastOffset = offset
+            } // onEnded ここまで
+    } // dragGesture ここまで
     
     // 画像の拡大縮小
     var scaleGuesture: some Gesture {
