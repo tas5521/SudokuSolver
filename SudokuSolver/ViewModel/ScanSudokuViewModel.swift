81cbee7b16fb758931a394e45a66acdbeb9b9e17
tas5521 <tsbs11152@gmail.com>
2024-03-10 16:12:47 +0900

    #33 青色の枠内を画像化する機能を追加

--- SudokuSolver/ViewModel/ScanSudokuViewModel.swift
+++ SudokuSolver/ViewModel/ScanSudokuViewModel.swift
@@ -14,6 +14,10 @@ final class ScanSudokuViewModel {
     var selectedPhoto: PhotosPickerItem? = nil
     // 取得したUIImageを保持する変数
     var image: UIImage? = nil
+    // フレームの座標とサイズの情報を保持する変数
+    var frameRect: CGRect = .zero
+    // 枠から切り抜いたImageを保持する変数
+    var imageInsideFrame: UIImage? = nil
     
     @MainActor
     // PhotosPickerItemをUIImageに変換
@@ -28,4 +32,16 @@ final class ScanSudokuViewModel {
             } // do-try-catch ここまで
         } // if let ここまで
     } // getUIImage ここまで
+    
+    // 枠内の画像を取得するメソッド
+    func getImageInsideFrame() {
+        // アプリケーションの最初のシーンを取得し、それがUIWindowSceneであることを確認
+        guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene else { return }
+        // 取得したウィンドウシーンから、最初のウィンドウを取得
+        guard let mainWindow = windowScene.windows.first else { return }
+        // ウィンドウのルートビューコントローラーのビューから、指定された枠内の画像を取得
+        guard let image = mainWindow.rootViewController?.view.getImage(rect: frameRect) else { return }
+        // 取得した画像を保持
+        imageInsideFrame = image
+    } // getImageInsideFrame ここまで
 } // ScanSudokuViewModel ここまで
